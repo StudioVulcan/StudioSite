d6e321868591b7e8271e2651c8bbb4681e9c043c
Zelif <jarodk.1988@gmail.com>
2018-02-04 23:23:26 +1100

    Js change
    - Changed function style

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){ 
+function ActivateSubmit(){
+	// Remove disabled from submit
+	document.getElementById("submit-request").disabled = false;
+}
+
+function HandleTermsChange(){
+	// Go get Radio button
 	var termsAndCond = document.getElementById("termsCheck1");
 	
-	termsAndCond.addEventListener('change', function(){
-		document.getElementById("submit-request").disabled = false;
-	});
-}, false);
+	// Subscribe to change event
+	termsAndCond.addEventListener('change', ActivateSubmit);
+}
+
+// Subscribe to when the page finishes loading
+document.addEventListener('DOMContentLoaded', HandleTermsChange , false);
