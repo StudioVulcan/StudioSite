5fc87068522d6b86febcddb9a57624835caefc8c
Zelif <jarodk.1988@gmail.com>
2018-02-05 20:01:46 +1100

    Events for Radios
    Added events to radio buttons to show textbox when clicked
    Changed Jquery to non slim version to use slide

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,3 @@
-function popOutBox(){
-	// pop-out box for 'find me' options
-	document.getElementById("findMeBox1").invisible = false;
-}
-
-
 function ActivateSubmit(){
 	// Remove disabled from submit
 	document.getElementById("submit-request").disabled = false;
@@ -17,5 +11,25 @@ function HandleTermsChange(){
 	termsAndCond.addEventListener('change', ActivateSubmit);
 }
 
+function HandleFindTextbox(){
+	$(".find-me-section .custom-radio").on("click",function(){
+		// Get the radio buttons
+		var radio = $(this).find('input');
+		// Check to see if the radio button is right
+		if(radio.val() === "Twitchtv" || radio.val() === "Other" ){
+			$("#find-textbox").slideDown();
+		} else{
+			$("#find-textbox input").val("");
+			$("#find-textbox").slideUp();
+		}
+	});
+}
+
+// Bind all events 
+function BindAllEvents(){
+	HandleTermsChange();
+	HandleFindTextbox();
+}
+
 // Subscribe to when the page finishes loading
-document.addEventListener('DOMContentLoaded', HandleTermsChange , false);
+document.addEventListener('DOMContentLoaded', BindAllEvents , false);
